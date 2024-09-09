c0eb8d3af98b5ee8c070db810285dfdad67d4b36
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-09-09 21:10:01 +0900

    7. Builder

---
+++ src/main/java/org/example/design_pattern/builder_7/Director.java
@@ -0,0 +1,20 @@
+package org.example.design_pattern.builder_7;
+
+public class Director {
+
+    private final Builder builder;
+
+    public Director(Builder builder) {
+        this.builder = builder;
+    }
+
+    public void construct() {
+        builder.makeTitle("Greeting");
+        builder.makeString("일반적인 인사");
+        builder.makeItems(new String[]{"How are you?","Hello","Hi"});
+        builder.makeString("시간대별 인사");
+        builder.makeItems(new String[]{"Good morning", "Good afternoon", "Good evening"});
+        builder.close();
+    }
+
+}

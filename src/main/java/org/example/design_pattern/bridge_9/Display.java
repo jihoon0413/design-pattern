d7bdfd679e5420f04b527bef2613ca938cd9617b
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-09-24 21:43:11 +0900

    9. Bridge

---
+++ src/main/java/org/example/design_pattern/bridge_9/Display.java
@@ -0,0 +1,29 @@
+package org.example.design_pattern.bridge_9;
+
+public class Display {
+
+    private DisplayImpl impl;
+
+    public Display(DisplayImpl impl) {
+        this.impl = impl;
+    }
+
+    public void open() {
+        impl.rawOpen();
+    }
+
+    public void print() {
+        impl.rawPrint();
+    }
+
+    public void close() {
+        impl.rawClose();
+    }
+
+    public final void display() {
+        open();
+        print();
+        close();
+    }
+
+}

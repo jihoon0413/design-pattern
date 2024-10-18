7ef968d7260e24d6c3cb77163b7698ecf672a969
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-10-18 20:36:31 +0900

    11. Composite

---
+++ src/main/java/org/example/design_pattern/composite_11/Entry.java
@@ -0,0 +1,19 @@
+package org.example.design_pattern.composite_11;
+
+public abstract class Entry {
+
+    public abstract String getName();
+
+    public abstract int getSize();
+
+    public void printList() {
+        printList("");
+    }
+
+    protected abstract void printList(String prefix);
+
+    @Override
+    public String toString() {
+        return getName() + " (" + getSize() + ")";
+    }
+}

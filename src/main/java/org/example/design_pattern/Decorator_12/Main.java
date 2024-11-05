e975821130b38b4bbfeecd54b0c9a85ff2db6744
jihoon0413 <119675297+jihoon0413@users.noreply.github.com>
2024-11-05 16:14:37 +0900

    12. Decorator
    연습 문제 풀이

--- src/main/java/org/example/design_pattern/Decorator_12/Main.java
+++ src/main/java/org/example/design_pattern/Decorator_12/Main.java
@@ -2,8 +2,11 @@ package org.example.design_pattern.Decorator_12;
 
 public class Main {
     public static void main(String[] args) {
-        Display b1 = new StringDisplay("Hello, world.");
-        Display b2 = new SideBorder(b1, '#');
+        MultiStringDisplay b1 = new MultiStringDisplay();
+        b1.add("HI!");
+        b1.add("Good morning");
+        b1.add("Good night!");
+        Display b2 = new UpDownBorder(b1, '-');
         Display b3 = new FullBorder(b2);
         b1.show();
         b2.show();

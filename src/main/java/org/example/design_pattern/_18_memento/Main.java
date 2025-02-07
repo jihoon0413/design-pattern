882f752f8ac5518c26bf7ef099747b8d2daabdee
jihoon <wlgnstls0413@naver.com>
2025-02-07 16:06:28 +0900

    18. Memento
    연습 문제 풀이

--- src/main/java/org/example/design_pattern/_18_memento/Main.java
+++ src/main/java/org/example/design_pattern/_18_memento/Main.java
@@ -4,10 +4,24 @@ import org.example.design_pattern._18_memento.game.Gamer;
 import org.example.design_pattern._18_memento.game.Memento;
 
 public class Main {
+    public static final String SAVEFILENAME = "game.dat";
     public static void main(String[] args) {
 
         Gamer gamer = new Gamer(100);
-        Memento memento = gamer.createMemento();
+
+        Memento memento = Memento.loadFromFile(SAVEFILENAME);
+
+//        Memento memento = gamer.createMemento();
+
+
+        if(memento == null) {
+            System.out.println("새로 시작합니다.");
+            memento = gamer.createMemento();
+        } else {
+            System.out.println("이전에 저장한 결과부터 시작합니다.");
+            gamer.restoreMemento(memento);
+        }
+
 
         for (int i = 0; i < 100; i++) {
             System.out.println("====" + i);
@@ -20,6 +34,9 @@ public class Main {
             if(gamer.getMoney() > memento.getMoney()) {
                 System.out.println("*많이 늘었으니 현재 상태를 저장하자!");
                 memento = gamer.createMemento();
+                if(Memento.saveToFile(SAVEFILENAME, memento)) {
+                    System.out.println("현재 상태를 파일로 저장했습니다.");
+                }
             } else if (gamer.getMoney() < memento.getMoney() / 2) {
                 System.out.println("*많이 줄었으니 이전 상태를 복원하자!");
                 gamer.restoreMemento(memento);
--- src/main/java/org/example/design_pattern/_18_memento/game/Memento.java
+++ src/main/java/org/example/design_pattern/_18_memento/game/Memento.java
@@ -1,5 +1,11 @@
 package org.example.design_pattern._18_memento.game;
 
+import org.example.design_pattern._11_composite.File;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +29,58 @@ public class Memento {
 
     List<String> getFruits() {
         return new ArrayList<>(fruits);
+    }
+
+    public static boolean saveToFile(String filename, Memento memento) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("%d", memento.money));
+        sb.append("\n");
+
+        for(String f: memento.getFruits()) {
+            sb.append(f);
+            sb.append("\n");
+        }
+
+        try {
+            Files.writeString(Path.of(filename), sb,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING,
+                    StandardOpenOption.WRITE);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return false;
+        }
+        return true;
+    }
+
+    public static Memento loadFromFile(String filename) {
+
+        try {
+            List<String> lines = Files.readAllLines(Path.of(filename));
+            if(lines.isEmpty()) {
+                System.out.println("Empty file");
+                return null;
+            }
+
+            int money = 0;
+            try {
+                money = Integer.parseInt(lines.get(0));
+            } catch (NumberFormatException e) {
+                System.out.println("Format error : " + e);
+                return null;
+            }
+
+            Memento memento = new Memento(money);
+
+            for (int i = 1; i < lines.size(); i++) {
+                memento.addFruits(lines.get(i));
+            }
+            return memento;
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            return null;
+        }
     }
 
 }

2f4855669e4dd88405d1282799b8c1830b622fc7
jihoon <wlgnstls0413@naver.com>
2025-02-25 10:50:03 +0900

    22. Command

---
+++ src/main/java/org/example/design_pattern/_22_command/command/Command.java
@@ -0,0 +1,5 @@
+package org.example.design_pattern._22_command.command;
+
+public interface Command {
+    public abstract void execute();
+}
---
+++ src/main/java/org/example/design_pattern/_22_command/drawer/Drawable.java
@@ -0,0 +1,5 @@
+package org.example.design_pattern._22_command.drawer;
+
+public interface Drawable {
+    public abstract void draw(int x, int y);
+}
